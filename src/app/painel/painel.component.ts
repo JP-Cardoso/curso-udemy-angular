9d0e638d820214bf81865465ebfa757be028a8be
gabs.renan <xiubanido@gmail.com>
2023-01-13 11:29:33 -0300

    pegando o valor dos eventos

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FRASES } from './frases-mock';
+import { Frase } from '../shared/frase.model';
 
 @Component({
   selector: 'app-painel',
@@ -7,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit{
 
+  public frases: Frase[] = FRASES;
+  public instrucao: string = 'Traduza a frase:';
+  public resposta!: string;
+
   constructor() {
-    
-  }
+    console.log(this.frases)
+  };
 
   ngOnInit(): void {
 
-  }
+  };
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = ((<HTMLInputElement>resposta.target).value);
+
+    console.log(this.resposta);
+  };
 
 }
